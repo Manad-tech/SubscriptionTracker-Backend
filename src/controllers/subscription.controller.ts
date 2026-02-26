31569e5c9d55126bc015bd417a7866c388180f5f
Manad-Tech <joshimanad81@gmail.com>
2026-02-26 18:34:04 +0530

    added: POST and GET api's

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -48,12 +48,47 @@ export const createSubscription = async (req: Request, res: Response) => {
 
 export const readSubscription = async (req: Request, res: Response) => {
   try {
-    const { userId } = req.query;
+    const { userId } = req.params;
 
     if (!userId) {
       return res.status(400).json({ message: "UserId is required" });
     }
 
-    
-  } catch (error) {}
+    const subscriptions = await Subscription.find({ userId });
+
+    let monthlyTotal = 0;
+    let yearlyTotal = 0;
+
+    for (let i = 0; i < subscriptions.length; i++) {
+      const sub: any = subscriptions[i];
+
+      if (sub.billingCycle === "Monthly") {
+        monthlyTotal = monthlyTotal + sub.amount;
+      } else if (sub.billingCycle === "Yearly") {
+        yearlyTotal = yearlyTotal + sub.amount;
+      }
+    }
+
+    return res.status(200).json({
+      subscriptions,
+      monthlyTotal,
+      yearlyTotal,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
+
+export const updateSubscription = async (req: Request , res:Response) => {
+  try {
+    const {userId} = req.params
+
+    if(!userId) {
+      return res.status(400).json({ message: 'UserId is required'})
+    }
+
+    const subscriptions = await Subscription.updateOne({userId:{} } , )
+  } catch (error) {
+    
+  }
+}
--- src/routes/subscription.routes.ts
+++ src/routes/subscription.routes.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import { createSubscription } from '../controllers/subscription.controller.js'
+import { createSubscription, readSubscription } from '../controllers/subscription.controller.js'
 
 const router = Router()
 
 router.post('/subscriptions' , createSubscription)
+router.get('/subscriptions/:userId' , readSubscription)
 
 export default router
